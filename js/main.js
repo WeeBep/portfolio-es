889484d2689273b61adf23f389d32b728a1cecb4
Ezequiel <ezequielperez64@gmail.com>
2018-12-09 06:07:09 -0300

    Primera Subida

---
+++ js/main.js
@@ -0,0 +1,32 @@
+// Smoothscroll - doc: https://github.com/cferdinandi/smooth-scroll
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 700,
+    speedAsDuration: true
+});
+
+// Scroll out - doc: https://scroll-out.github.io/guide.html#configuration
+ScrollOut();
+
+const servicios = document.querySelector(".servicios");
+const trabajos = document.querySelector(".primero");
+const about = document.querySelector(".about-info");
+const menuButtons = document.querySelectorAll(".btn-menu");
+
+// Prender y apagar active
+window.addEventListener("scroll", () => {
+    if (trabajos.dataset.scroll == "in") {
+        menuButtons[0].classList.remove("active");
+        menuButtons[2].classList.remove("active");
+        menuButtons[1].classList.add("active");
+    }
+
+    if (servicios.dataset.scroll == "in") {
+        menuButtons[1].classList.remove("active");
+        menuButtons[0].classList.add("active");
+    }
+
+    if (about.dataset.scroll == "in") {
+        menuButtons[1].classList.remove("active");
+        menuButtons[2].classList.add("active");
+    }
+});
